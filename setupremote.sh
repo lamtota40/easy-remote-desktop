34e97f915c44655dfdfc29c5504caf7b478ad60e
lamtota40 <lamtota40@gmail.com>
2023-01-27 15:16:03 +0700

    Update setupremote.sh

--- setupremote.sh
+++ setupremote.sh
@@ -12,8 +12,14 @@ sudo wget dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P 
 sudo apt install /tmp/chrome-remote-desktop_current_amd64.deb -y
 echo "and now open https://remotedesktop.google.com/access"
 
-sudo wget download.teamviewer.com/download/linux/teamviewer_amd64.deb -P /tmp
-sudo apt install /tmp/teamviewer_amd64.deb -y
-
 sudo wget https://www.nomachine.com/free/linux/64/deb -O nomachine.deb -P /tmp
 sudo apt install /tmp/nomachine.deb -y
+
+sudo wget download.teamviewer.com/download/linux/teamviewer_amd64.deb -P /tmp
+sudo apt install /tmp/teamviewer_amd64.deb -y
+sudo teamviewer license accept
+sudo chmod 777 /opt/teamviewer/config/global.conf
+sudo teamviewer daemon restart
+sudo teamviewer setup
+sudo teamviewer passwd pas123
+sudo teamviewer info
