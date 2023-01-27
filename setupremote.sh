08ca07f0a7e71ffbd8d864220e6acf7bccee7c1e
lamtota40 <lamtota40@gmail.com>
2023-01-27 09:34:19 +0700

    Update setupremote.sh

--- setupremote.sh
+++ setupremote.sh
@@ -1,2 +1,6 @@
 sudo apt install xrdp -y
 sudo usermod -a -G ssl-cert xrdp
+
+sudo wget dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb -P /tmp
+sudo apt install /tmp/chrome-remote-desktop_current_amd64.deb -y
+echo "and now open https://remotedesktop.google.com/access"
